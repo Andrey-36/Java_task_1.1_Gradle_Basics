0d2ecd413e62946965b1155d0367a419ff3f5460
Andrey Kolotukhin <KolotukhinAndrey@rambler.ru>
2022-03-15 19:37:55 +0300

    Initial commit

---
+++ src/test/java/ru/netology/unit/CashbackHackServiceTest.java
@@ -0,0 +1,19 @@
+package ru.netology.unit;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CashbackHackServiceTest {
+
+    @Test
+    void shouldRemain() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 900;
+
+        int actual = service.remain(amount);
+        int expected = 100;
+
+        assertEquals(expected, actual);
+    }
+}
