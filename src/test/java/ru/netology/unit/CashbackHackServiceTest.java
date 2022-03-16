06707cb23224b051b6a212d3e882be6fb2591d9b
Andrey Kolotukhin <KolotukhinAndrey@rambler.ru>
2022-03-16 20:55:05 +0300

    Added tests

--- src/test/java/ru/netology/unit/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/unit/CashbackHackServiceTest.java
@@ -6,13 +6,52 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class CashbackHackServiceTest {
 
-    @Test
-    void shouldRemain() {
-        CashbackHackService service = new CashbackHackService();
-        int amount = 900;
+    CashbackHackService service = new CashbackHackService();
 
-        int actual = service.remain(amount);
-        int expected = 100;
+    @Test
+    void shouldMessage1000Purchase0() {
+        int expected = 1000;
+        int actual = service.remain(0);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldMessage500PurchaseAbove1000() {
+        int expected = 500;
+        int actual = service.remain(1500);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldMessageLimitPurchaseAboveBoundary() {
+        int expected = 1000;
+        int actual = service.remain(2000);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldMessage1Purchase999() {
+        int expected = 1;
+        int actual = service.remain(999);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldMessage999Purchase1() {
+        int expected = 999;
+        int actual = service.remain(1);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldMessage500Purchase500() {
+        int expected = 500;
+        int actual = service.remain(500);
 
         assertEquals(expected, actual);
     }
