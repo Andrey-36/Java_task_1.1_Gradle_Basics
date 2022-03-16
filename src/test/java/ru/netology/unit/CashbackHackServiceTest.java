d3f95dbe3de98244e4de6e3b0d867f26a88dc207
Andrey Kolotukhin <KolotukhinAndrey@rambler.ru>
2022-03-16 21:44:07 +0300

    Added testng

--- src/test/java/ru/netology/unit/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/unit/CashbackHackServiceTest.java
@@ -1,19 +1,65 @@
 package ru.netology.unit;
 
-import org.junit.jupiter.api.Test;
+import org.testng.annotations.Test;
+import static org.junit.Assert.*;
 
-import static org.junit.jupiter.api.Assertions.*;
+public class CashbackHackServiceTest {
 
-class CashbackHackServiceTest {
+    CashbackHackService service = new CashbackHackService();
 
     @Test
-    void shouldRemain() {
-        CashbackHackService service = new CashbackHackService();
-        int amount = 900;
+    public void shouldMessage1000Purchase0() {
+        int expected = 1000;
+        int actual = service.remain(0);
 
-        int actual = service.remain(amount);
-        int expected = 100;
+        assertEquals(actual, expected);
+    }
 
-        assertEquals(expected, actual);
+    @Test
+    public void shouldMessage0Purchase1000() {
+        int expected = 0;
+        int actual = service.remain(1000);
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldMessage500PurchaseAbove1000() {
+        int expected = 500;
+        int actual = service.remain(1500);
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldMessageLimitPurchaseAboveBoundary() {
+        int expected = 1000;
+        int actual = service.remain(2000);
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldMessage1Purchase999() {
+        int expected = 1;
+        int actual = service.remain(999);
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldMessage999Purchase1() {
+        int expected = 999;
+        int actual = service.remain(1);
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldMessage500Purchase500() {
+        int expected = 500;
+        int actual = service.remain(500);
+
+        assertEquals(actual, expected);
     }
 }
